c9424d8691affc6bce49a231a17ca9ffd0255d60
Ethan Lauzon <ethan.lauzon@gmail.com>
2022-07-24 10:08:05 -0600

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,8 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Ethan Lauzon";
+    int age = 23;
+    String state = "Colorado";
+    System.out.println("Name:" + " "+name +" "+"Age:"+" "+age +" "+"State:"+" "+state);
+  }
+}
