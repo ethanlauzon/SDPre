370987fc2e4269b21d7280f10d6f7f05ae63aed1
Ethan Lauzon <ethan.lauzon@gmail.com>
2022-07-24 14:52:29 -0600

    complete objective3lab2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,8 @@
+public class Objective3Lab2 {
+  public static void main(String[] args) {
+    double side1 = 6 * 6;
+    double side2 = 7 * 7;
+    double hypotenuse = Math.sqrt(side1 + side2);
+    System.out.println(hypotenuse);
+  }
+}
