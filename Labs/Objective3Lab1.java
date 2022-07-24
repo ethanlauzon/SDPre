a478f6d98dcb8057567a9057633a6a7346bbf45e
Ethan Lauzon <ethan.lauzon@gmail.com>
2022-07-24 11:23:20 -0600

    complete objective3lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,9 @@
+public class Objective3Lab1 {
+    public static void main(String[] args) {
+      int num1 = 2;
+      int num2 = 6;
+      int num3 = 2;
+      double average =3.333333333;
+      System.out.println(average);
+    }
+}
