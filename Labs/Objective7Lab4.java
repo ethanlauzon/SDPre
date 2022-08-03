d31fd83e6a588bca7f8023eda359e375d745bfe6
Ethan Lauzon <ethan.lauzon@gmail.com>
2022-08-03 13:41:24 -0600

    complete objective7lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,13 @@
+public class Objective7Lab4{
+  public static void main(String[] args){
+
+    int count = 1;
+    int sum = 0;
+
+      do {
+         sum = sum + count;
+         count++;
+      } while(count<=20);
+      System.out.println(sum);
+  }
+}
